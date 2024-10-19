35f3bcc09e66c4865049b220ccfc1836ffabdf4e
pwscoding <pwscoding@gmail.com>
2024-10-19 23:33:13 +0530

    Updated

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     return (
         <footer className="bg-gray-800 text-white text-center py-4 mt-8">
             <p>
-                &copy; {currentYear} <a href="https://shadabdev.vercel.app/" className="text-blue-400 hover:underline" target="_blank" rel="noopener noreferrer">Shadab Khan</a>. All rights reserved.
+                &copy; {currentYear} <a href='' className="text-blue-400 hover:underline" target="_blank" rel="noopener noreferrer">Priyanka</a>. All rights reserved.
             </p>
         </footer>
     );
